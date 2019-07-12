a086a49e3a0ec4dbb01ffac16a24d480266da6a0
Eli Finkel <Eli.Finkel@checkmarx.com>
2019-07-12 09:42:07 +0300

    first

---
+++ Code.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace WebApiHello
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config) =>
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+    }
+
+    public class HelloController : ApiController
+    {
+        public string Get() => "Hello World";
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace WebApiHello
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start() => GlobalConfiguration.Configure(WebApiConfig.Register);
+    }
+}
